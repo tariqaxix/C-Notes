1a110cdd6dd8fc4c71b4f9d334490aa15ff3dc28
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-07 10:20:48 +0600

    Continue statment added

---
+++ Continue_statment.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int a = 10;
+  do 
+  {
+    if(a == 15)
+    {
+      a = a + 1;
+      continue;
+    }
+    printf("The value of a is %d\n", a);
+    a++;
+  } while (a < 20);
+  return 0;
+}
