92ac5669b3033863c4a49cfbbdbd4314b3598c0a
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-08 10:40:54 +0600

    User defined function added

---
+++ User_defined_function.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+int main()
+{
+  int n1, n2, sum;
+  printf("Enter two numbers: ");
+  scanf("%d %d", &n1, &n2);
+  sum = AddNumbers(n1, n2);
+  printf("The sum is %d", sum);
+  return 0;
+}
+int AddNumbers(int a, int b)
+{
+  int result;
+  result = a + b;
+  return result;
+}
+
+
+
+
+
