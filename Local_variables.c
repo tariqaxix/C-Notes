8d385ca5bea55b288877b5bc75a82b6aeae00fa1
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-09 09:34:01 +0600

    Local variables added

---
+++ Local_variables.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+int main()
+{
+  // Local variable declaration
+  int a, b; 
+  int c;
+  a = 10;
+  b = 20;
+  c = a + b;
+  printf("The value of a = %d, b = %d, and c = %d\n", a, b, c);
+  return 0;
+}
+
+
