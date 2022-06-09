9c58f87d58aa08d741534be67178ad948071a321
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-09 11:26:59 +0600

    Pass array to function added

--- Pass_array_to_function.c
+++ Pass_array_to_function.c
@@ -9,8 +9,11 @@ int main()
   char arr[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'h', 'i', 'j'};
   for(int x = 0; x < 10; x++)
   {
+    // Passing each element using subscript
     disp(arr[x]);
   }
   return 0;
 }
+
+
 
