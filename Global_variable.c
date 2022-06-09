81bddb464647b0a05781699de6ab21ad84467a78
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-09 10:36:02 +0600

    Global variable added

---
+++ Global_variable.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+// Global variable declaration
+int g;
+int main()
+{
+  // Local variable declaration
+  int a, b; 
+  a = 10;
+  b = 20;
+  g = a + b; 
+  printf("The value of a = %d, b = %d, and g = %d\n", a, b, g);
+  return 0;
+}
+
+
+
+
+
