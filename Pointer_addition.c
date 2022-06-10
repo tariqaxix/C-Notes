8e80767434e0da0953f51535d5c8fc8e0ce2d81f
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-10 12:32:07 +0600

    Pointer's addition

--- Pointer_addition.c
+++ Pointer_addition.c
@@ -4,8 +4,10 @@ int main()
 {
   int var = 50;
   int *p;
+  // Sore the adress of number variable
   p = &var;
   printf("The adress of p variable is %x\n", p);
+  // Adding 3 to pointer variable
   p = p + 3;
   printf("After adding 3: Adress of p variable is %x\n", p);
   return 0;
