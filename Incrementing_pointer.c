b7db74df535b2060f10566f385f288a3a50e7203
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-10 11:45:19 +0600

    Incrementing a pointer

---
+++ Incrementing_pointer.c
@@ -0,0 +1,19 @@
+
+#include <stdio.H>
+const int MAX = 3;
+int main()
+{
+  int var[] = {10, 100, 200};
+  int i, *ptr;
+  // Array adress in pointer
+  ptr = &var;
+  for(i = 0; i < MAX; i++)
+  {
+    printf("The adress of var[%d] is %x\n", i, ptr);
+    printf("The value of var[%d] = %d\n", i, *ptr);
+    // Move to the next location
+    ptr++;
+  }
+  return 0;
+}
+
