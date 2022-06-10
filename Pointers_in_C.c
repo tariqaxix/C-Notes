c5cfe6f5d16546a4cca22db78493af9bcc91b355
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-10 11:27:47 +0600

    Pointers added

---
+++ Pointers_in_C.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+int main()
+{
+  int var = 20;
+  // Pointer variable declaraton
+  int *ip;
+  ip = &var;
+  printf("Adress of var variable is %x\n", &var);
+  // Adress stored in variable 
+  printf("Adress stored in ip variable is %x\n", ip);
+  // Access the value using pointer
+  printf("Value of *ip variable is %d\n", *ip);
+  return 0;
+}
+
+
