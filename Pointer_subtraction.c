caa94414b50f655888a13f11b6c99b8407505c82
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-11 11:41:52 +0600

    Pointer's subtraction added

--- Pointer_subtraction.c
+++ Pointer_subtraction.c
@@ -6,6 +6,7 @@ int main()
   int *p;
   p = &var;
   printf("Adress of p variable is %x\n", p);
+  // Subtracting 3 from pointer variable
   p = p - 3;
   printf("After subtracting 3: The adress of p variable is %x\n", p);
   return 0;
