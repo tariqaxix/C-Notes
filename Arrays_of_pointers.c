1a545a1d579bd8f474027da269a2b0dd23a8ac3e
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-11 12:01:26 +0600

    Array of pointer added

---
+++ Arrays_of_pointers.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+const int MAX = 3;
+int main()
+{
+  int var[] = {10, 100, 200};
+  int i, *ptr[MAX];
+  for(i = 0; i < MAX; i++)
+  {
+    ptr[i] = &var[i];
+
+  }
+  for(i = 0; i < MAX; i++)
+  {
+    printf("The adress of var[%d] = %x\n", i, ptr[i]);
+    printf("The value of var[%d] = %d\n", i, *ptr[i]);
+  }
+  return 0;
+}
+
+
