984f8b93829fe303bde80a3913930e531bfe833e
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-11 11:49:47 +0600

    Pointer comparison added

--- Pointers_comparison.c
+++ Pointers_comparison.c
@@ -11,6 +11,7 @@ int main()
   {
     printf("Adress of var[%d] = %x\n", i, ptr);
     printf("The value of var[%d] = %d\n", i, *ptr);
+    // Pointer to previous location
     ptr++;
     i++;
   }
