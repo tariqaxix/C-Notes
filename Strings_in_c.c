f588e87aebb938a5103b6dbfd80ea732f75f30b2
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-12 12:16:47 +0600

    Strings in C added

---
+++ Strings_in_c.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main()
+{
+  char a[6] = {'H', 'E', 'L', 'L', 'O', '\0'};
+  char b[] = "Hello";
+  printf("The greeting message s %s\n", a);
+  printf("The greeting message is %s\n", b);
+  return 0;
+}
+
+
