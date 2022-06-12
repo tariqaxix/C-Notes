b5914c37c7819aaabf770125934c11ca8943e3e3
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-12 12:21:55 +0600

    Read line of text added

---
+++ Read_line_of_text.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+int main()
+{
+  char university[20];
+  printf("Enter the name of your university: ");
+  scanf("%s", university);
+  printf("Your university is %s.", university);
+  return 0;
+}
+
