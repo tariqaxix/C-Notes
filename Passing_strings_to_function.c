04f5010c9c4f7df5fa5183b5ef8698627fdb9bf3
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-12 12:29:47 +0600

    Passing string to function added

---
+++ Passing_strings_to_function.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+void displayString(char str[]);
+int main()
+{
+  char str[50];
+  printf("Enter the string: ");
+  fgets(str, sizeof(str), stdin);
+  displayString(str);
+  return 0;
+}
+void displayString(char str[])
+{
+  printf("String output: ");
+  puts(str);
+}
+
