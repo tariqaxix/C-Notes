328d6aeb6ff1244bbb2a50231da880f7b4f7506d
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-12 12:55:22 +0600

    Strings manipulation using libraries added

---
+++ Strings_manipulation_using_library.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+  char str1[12] = "Hello";
+  char str2[12] = "World";
+  char str3[12];
+  int len;
+  // Copy str1 into str3
+  strcpy(str3, str1);
+  printf("strcpy(str3, str1) : %s\n", str3);
+  // Concatenate str1 and str3
+  strcat(str1, str2);
+  printf("strcat(str1, str2) : %s\n", str1);
+  // Total length of str1 afer concatenation
+  len = strlen(str1);
+  printf("strlen(str1) : %d\n", len);
+  return 0;
+}
+
+
