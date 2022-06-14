bc0f2a7967df6a2f3254c2250bf0a7ce3e928157
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-14 10:44:51 +0600

    Accessing union members added

---
+++ Accessing_union_members.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+union Job 
+{
+  float salary;
+  int workerNo;
+} j;
+int main()
+{
+  j.salary = 50.6;
+  printf("Salary = %.1f\n", j.salary);
+  j.workerNo = 33;
+  printf("Number of worker is = %d", j.workerNo);
+  return 0;
+}
+
+
