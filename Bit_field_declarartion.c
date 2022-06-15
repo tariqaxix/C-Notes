0c0723163dc563a8fef0fcd08756360aaf729b05
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-15 13:24:12 +0600

    Bit field decalaration added

---
+++ Bit_field_declarartion.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+struct {
+  unsigned int age : 3;
+} Age;
+int main()
+{
+  Age.age = 4;
+  printf("Sizeof( Age ) : %d\n", sizeof(Age));
+  printf("Age.age : %d\n", Age.age);
+  Age.age = 7;
+  printf("Age.age : %d\n", Age.age);
+  Age.age = 8;
+  printf("Age.age : %d\n", Age.age);
+  return 0;
+}
+
+
