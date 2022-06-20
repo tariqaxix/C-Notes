bba62b20f9fde8aee92879d55ca435516b222753
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-20 11:24:16 +0600

    Pre-defined Macros added

---
+++ Pre-defined_Macros.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main()
+{
+  printf("File : %s\n", __FILE__);
+  printf("Date : %s\n", __DATE__);
+  printf("Time : %s\n", __TIME__);
+  printf("Line : %s\n", __LINE__);
+  printf("ANSI : %s\n", __STDC__);
+  return 0;
+}
+
