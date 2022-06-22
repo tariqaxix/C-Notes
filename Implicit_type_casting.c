41e9446dc75c60c0ba1cf483f1fd25efca7df948
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-22 10:58:43 +0600

    Implicit type casting added

---
+++ Implicit_type_casting.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main()
+{
+  // Initializing variable of data type short
+  short a = 10;
+  // Declaring int variable 
+  int b;
+  // implicit type casting
+  b = a;
+  printf("%d\n", a);
+  printf("%d\n", b);
+  return 0;
+}
+
+
