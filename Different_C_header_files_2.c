1e702734b68dd0bc5f71cf85d2e08549c7d01ae5
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-22 10:47:40 +0600

    Different C header files 2 added

---
+++ Different_C_header_files_2.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  printf("The value of 8.0 ^ 3 = %lf\n", pow(8.0, 3));
+  printf("The value of 3.05 ^ 1.98 = %lf\n", pow(3.05, 1.95));
+  return 0;
+}
+
+
