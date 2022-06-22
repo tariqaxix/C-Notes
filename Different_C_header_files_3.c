5f62d3498891a4735055f24129fc0396191576ea
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-22 10:52:16 +0600

    Different C header files 3 added

---
+++ Different_C_header_files_3.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  int a, b;
+  a = abs(5);
+  printf("The value of a = %d\n", a);
+  b = abs(-10);
+  printf("The value of b = %d\n", b);
+  return (0);
+}
+
+
