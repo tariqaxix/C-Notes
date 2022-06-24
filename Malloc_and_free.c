e6fc29d089bd69bc8eae91067c69425af9460374
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-24 11:42:49 +0600

    Malloc and free added

---
+++ Malloc_and_free.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  int n, i, *ptr, sum = 0;
+  printf("Enter the number of elements: ");
+  scanf("%d", &n);
+  ptr = (int*) malloc(n *sizeof(int));
+  // If memory can't be allocated
+  if(ptr == NULL)
+  {
+    printf("Error! Memory not allocated.");
+    exit(0);
+  }
+  printf("Enter the elements: ");
+  for(i = 0; i < n; ++i)
+  {
+    scanf("%d", ptr + 1);
+    sum += *(ptr + 1);
+  }
+  printf("Sum = %d", sum);
+  // Deallocating memory
+  free(ptr);
+  return 0;
+}
+
