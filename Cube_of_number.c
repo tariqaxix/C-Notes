57228e6513bbf5c88b031c6ba10740b0f6c880e4
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-25 15:45:35 +0600

    Cube of number added

---
+++ Cube_of_number.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main()
+{
+  float a, cube;
+  printf("Enter the number: ");
+  scanf("%f", &a);
+  cube = a*a*a;
+  printf("The cube of %f\n is ", cube);
+  return 0;
+}
+
+
