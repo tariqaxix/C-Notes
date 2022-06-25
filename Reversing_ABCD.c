6aa0b1ee809bdea6a580b6ae595b4479d2c3dd72
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-25 15:41:20 +0600

    Reversing order of ABCD added

---
+++ Reversing_ABCD.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main()
+{
+  char a = 'A';
+  char b = 'B';
+  char c = 'C';
+  char d = 'D';
+  printf("Reverse order of %c%c%c%c is %c%c%c%c\n", a,b,c,d, d,c,b,a);
+  return 0;
+}
+
+
