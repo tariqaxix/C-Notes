951cebd1ff664c78be9b5868e5c7c1fde233d8fc
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-27 11:11:04 +0600

    Size of long data type added

---
+++ Size_of_long_data_type.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main()
+{
+  int a;
+  long b;
+  printf("Size of int = %ld bytes \n", sizeof(a));
+  printf("Size of long = %ld bytes \n", sizeof(b));
+  return 0;
+}
+
+
