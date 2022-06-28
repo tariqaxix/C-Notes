994ae53a91443af4470f2579bb25fe502e96cf07
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-28 11:38:18 +0600

    Operational operators added

---
+++ Assignments/Relational_operations.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+int main()
+{
+  int a, b, C;
+  printf("Enter the first number: ");
+  scanf("%d", &a);
+  printf("Enter the second number: ");
+  scanf("%d", &b);
+  printf("Enter the third number: ");
+  scanf("%d", &C);
+  printf("%d == %d is %d \n",a, b, a == b);
+  printf("%d == %d is %d \n",a, C, a == C);
+  printf("%d > %d is %d \n",a, b, a > b);
+  printf("%d > %d is %d \n",a, C, a > C);
+  printf("%d < %d is %d \n",a, b, a < b);
+  printf("%d < %d is %d \n",a, C, a < C);
+  printf("%d != %d is %d \n",a, b, a != b);
+  printf("%d != %d is %d \n",a, C, a != C);
+  printf("%d >= %d is %d \n",a, b, a >= b);
+  printf("%d >= %d is %d \n",a, C, a >= C);
+  printf("%d <= %d is %d \n",a, b, a <= b);
+  printf("%d <= %d is %d \n",a, C, a <= C);
+  return 0;
+}
+
+
