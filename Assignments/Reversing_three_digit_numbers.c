c009135de5d6ab188073409f2ba9f7600b875c5c
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-28 11:26:06 +0600

    Reversing a three digit number added

---
+++ Assignments/Reversing_three_digit_numbers.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+int main(void) {
+  int n = 0;
+  printf("Enter a three digit number: ");
+  scanf("%d", &n);
+  printf("The reverse of given number is %d%d%d\n", n % 10, n / 10 % 10, n / 100);
+  return 0;
+}
+
+
