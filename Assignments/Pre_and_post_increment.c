33b2c9caaac97811bd3eed3b6921609c0622096f
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-28 11:21:37 +0600

    Pre and post incerement added

---
+++ Assignments/Pre_and_post_increment.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+int main()
+{
+  int i = 4;
+  // Post-increment
+  printf("%d\n", i++);
+  printf("%d\n", i);
+  // Pre-increment
+  printf("%d\n", ++i);
+  printf("%d\n", i);
+  return 0;
+}
+
