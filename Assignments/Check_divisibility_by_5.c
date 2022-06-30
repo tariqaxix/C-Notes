5829746f17b3b4eb593ac81a4100dd4010be07ba
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-30 11:32:25 +0600

    Check the divisibility by 5 added

---
+++ Assignments/Check_divisibility_by_5.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+int main()
+{
+  int n;
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  if(n % 5 == 0) {
+    printf("%d is divisible by 5", n);
+  }
+  else 
+  {
+    printf("%d is not divisible by 5");
+  }
+  return 0;
+}
+
+
