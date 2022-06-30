985198adc8b2ee6d42093fc8611dcf48072ef223
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-06-30 11:07:07 +0600

    Absolute value of number added

---
+++ Assignments/Calculate_absolute_value.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  int m;
+  printf("Enter an integer: ");
+  scanf("%d", &m);
+  printf("The absolute value is %d\n", abs(m));
+  return 0;
+}
+
+
