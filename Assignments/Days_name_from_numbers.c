3f96fd571fc7a9e5ce1a5328b4723c2dba1fa917
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-07-01 11:12:31 +0600

    Days name using numbers added

---
+++ Assignments/Days_name_from_numbers.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+int main()
+{
+  int wDays;
+  printf("Enter a week day number (0-6): ");
+  scanf("%d", &wDays);
+  switch (wDays)
+  {
+  case 0:
+  printf("Sunday");
+    break;
+  case 1:
+  printf("Monday");
+    break;
+  case 2:
+  printf("Tuesday");
+    break;
+  case 3:
+  printf("Wednesday");
+    break;
+  case 4:
+  printf("Thursday");
+    break;
+  case 5:
+  printf("Friday");
+    break;
+  case 6:
+  printf("Saturday");
+    break;
+  }
+  return 0;
+}
+
+
