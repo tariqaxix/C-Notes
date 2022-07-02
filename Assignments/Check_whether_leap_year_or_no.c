1ff2e898e626993df6b3ab93a58f4ac8cf838aef
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-07-02 16:06:42 +0600

    Check whether leap year or no using switch statment

---
+++ Assignments/Check_whether_leap_year_or_no.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+int main()
+{
+  int year;
+  printf("Enter the year: ");
+  scanf("%d", &year);
+  if (year % 4 == 0) 
+  {
+    if (year % 100 == 0) 
+    {
+      // Year is divisible by 400, hence year is a leap year
+      if (year % 400 == 0) 
+      {
+        printf("%d is a leap year", year);
+      }
+      else 
+      {
+        printf("%d is not a leap year", year);
+      }
+    }
+    else 
+    {
+      printf("%d is a leap year", year);
+    }
+  }
+  else 
+  {
+    printf("%d is not a leap year", year);
+  }
+  return 0;
+}
+
+
