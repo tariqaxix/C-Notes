6a9d3190a04624e7084e06d2899dd5884a4ccb71
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-07-05 11:34:58 +0600

    Check whether the number is divisible by A and B added

---
+++ Assignments/Check_whether_divisible-by_A_or_B.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+int CheckDivision(int num, int a, int b)
+{
+  if (num % a == 0 && num % b == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+int main()
+{
+  int number = 0, A = 0, B = 0;
+  printf("Enter any integer number: ");
+  scanf("%d", &number);
+  printf("Enter first divisor: ");
+  scanf("%d", &A);
+  printf("Enter second divisor: ");
+  scanf("%d", &B);
+  if (CheckDivision(number, A, B))
+  {
+    printf("%d is divisible by %d and %d\n", number, A, B);
+  }
+  else 
+  {
+    printf("%d is not divisible by %d and %d\n", number, A, B);
+  }
+  return 0;
+}
+
+
