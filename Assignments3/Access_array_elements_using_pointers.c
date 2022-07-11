293753dd1ddec757e8ec8bce8ba631ffb3a2153a
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-07-11 13:45:19 +0600

    Access array elements using pointers added

---
+++ Assignments3/Access_array_elements_using_pointers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  int data[5];
+  printf("Enter the elements: ");
+  for (int i = 0; i < 5; ++i)
+  {
+    scanf("%d", data + i);
+  }
+  printf("You entered: \n");
+  for (int i = 0; i < 5; ++i)
+  {
+    printf("%d\n", *(data + i));
+  }
+  return 0;
+}
+
+
