f071f0d6a3d6be62d4136e604d1c00c1d09d6527
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-07-11 13:50:27 +0600

    Size of different data types of pointer variables added

---
+++ Assignments3/Size_of_pointer_variables.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main()
+{
+  printf("\nSize of char pointer is %d",   sizeof(char*));
+  printf("\nSize of int pointer is %d",   sizeof(int*));
+  printf("\nSize of float pointer is %d",   sizeof(float*));
+  printf("\nSize of long int pointer is %d",   sizeof(long int*));
+  printf("\nSize of double pointer is %d\n",   sizeof(double*));
+  return 0;
+}
+
+
