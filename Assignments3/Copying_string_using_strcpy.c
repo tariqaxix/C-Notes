980222f99a43d6bcd23b9361a2520c2f0f7d0bd7
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-07-13 11:36:43 +0600

    Copying string to another string using strcpy() added

---
+++ Assignments3/Copying_string_using_strcpy.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <string.h>
+// Maximum size of the string
+#define MAX_SIZE 100
+int main()
+{
+  char text1[MAX_SIZE], text2[MAX_SIZE];
+  // Input original string from the user
+  printf("Enter any string : ");
+  fgets(text1, MAX_SIZE, stdin);
+  // Copy text1 to text2 using strcpy()
+  strcpy(text2, text1);
+  printf("First string is %s\n", text1);
+  printf("Second string is %s\n", text2);
+  return 0;
+}
+
+
+
