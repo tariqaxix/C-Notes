8860c016d8bc450c6389dac7e18c2f4409a47621
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-07-15 11:31:08 +0600

    Remove characters from the strings except for the alphabets added

---
+++ Assignments3/Remove_characters_from_string_except_alphabets.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+int main()
+{
+  char line[150];
+  int i, j;
+  printf("Enter a string : ");
+  fgets(line, 150, stdin);
+  for (i = 0; line[i] != '\0'; ++i)
+  {
+    while (!((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z') || line[i] == '\0'))
+    {
+      for (j = i; line[j] != '\0'; ++j)
+      {
+        line[j] = line[j + 1];
+      }
+      line[j] = '\0';
+    }
+  }
+  printf("Output string : ");
+  puts(line);
+  return 0;
+}
+
+
+
