5791c8f78faf3ade90a85b2feab532087d2de39a
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-07-20 10:53:33 +0600

    Demonstrate the use of Macro __LINE__ added

---
+++ Assignments4/Demonstrate_use_of_Macro_line.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main()
+{
+  printf("Hello World\n");
+  printf("Line : %d\n", __LINE__);
+  printf("How are you?\n");
+  printf("Line : %d\n", __LINE__);
+  printf("Bye Bye!!!\n");
+  return 0;
+}
+
+
