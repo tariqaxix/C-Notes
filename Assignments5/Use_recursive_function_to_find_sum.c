7c26aa3a44700a2792c648fe5b6da7b50b223732
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-07-23 17:37:31 +0600

    Use of recursive function to find the sum added

---
+++ Assignments5/Use_recursive_function_to_find_sum.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+int sum(int n)
+{
+  if (n != 0)
+    // sum() function calls itself
+    return n + sum(n-1);
+  else 
+    return n;
+}
+int main()
+{
+  int number, result;
+  printf("Enter a positive integer : ");
+  scanf("%d", &number);
+  result = sum(number);
+  printf("Sum is %d", result);
+  return 0;
+}
+
+
